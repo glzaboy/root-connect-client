bf28358d58b3bda0340b29910064e930432979c5
guliuzhong <glzaboy@163.com>
2019-06-10 00:38:33 +0800

    优化gitignore 初始化项目。

---
+++ src/main/java/com/qintingfm/root/connect/client/DiscardServerHandler.java
@@ -0,0 +1,19 @@
+package com.qintingfm.root.connect.client;
+
+import com.qintingfm.root.connect.server.pojo.User;
+import io.netty.channel.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+public class DiscardServerHandler extends ChannelHandlerAdapter {
+    Logger logger= LoggerFactory.getLogger(DiscardServerHandler.class);
+
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        User msg1 = (User) msg;
+        msg1.setDate(new Date());
+        ChannelFuture channelFuture = ctx.writeAndFlush(msg1);
+        channelFuture.addListener(ChannelFutureListener.CLOSE);
+    }
+}
---
+++ src/main/java/com/qintingfm/root/connect/client/pojo/User.java
@@ -0,0 +1,52 @@
+package com.qintingfm.root.connect.client.pojo;
+
+import java.util.Date;
+
+public class User {
+    String name ;
+    String sex;
+    String ip;
+    Date date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", ip='" + ip + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
